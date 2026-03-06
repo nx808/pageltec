f3c58944432d63131a08c9707b4cafe86e773874
nx808 <lucio.pagel@gmail.com>
2026-03-07 00:42:20 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
+  reactStrictMode: true,
+  output: 'export',    // WICHTIG: Erzeugt den 'out' Ordner
+  images: {
+    unoptimized: true, // Notwendig, da Hostinger Webhosting keine Server-Seite Image-Optimierung hat
+  },
 };
 
-export default nextConfig;
+export default nextConfig;https://github.com/nx808/pageltec/blob/main/next.config.ts
